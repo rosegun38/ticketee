618446096d383f8fdd66f50e6c8af6c7e2f24a5d
jian.ma <jian.ma@dianrong.com>
2016-05-02 14:13:52 +0800

    Add the project model.

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,18 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'projects#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id' => 'ca
+  # talog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resource :projects
 
   # Example resource route with options:
   #   resources :products do
